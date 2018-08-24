0723e8553265d5e38c3065bf20e401d5be373c94
darkfriend77 <darkfriend@swissonline.ch>
2018-08-24 18:31:25 +0200

    working on the first sheep fight

--- WoMInterface/Game/Combat.cs
+++ WoMInterface/Game/Combat.cs
@@ -8,9 +8,11 @@ namespace WoMInterface.Game
 {
     class Combatant
     {
+        public bool IsHero { get; set; } = false;
         public Entity Entity { get; }
         public int InititativeValue { get; set; }
         public Dice Dice { get; set; }
+        public List<Entity> Enemies { get; set; }
         
         public Combatant(Entity entity)
         {
@@ -20,23 +22,35 @@ namespace WoMInterface.Game
     }
     class Combat
     {
+        private int maxRounds;
         private List<Combatant> InititiveOrder;
 
-        public Combat(Mogwai mogwai, Dice mogwaiDice, Monster monster, Dice monsterDice)
+        public Combat(Mogwai mogwai, Dice mogwaiDice, Monster monster, Dice monsterDice, int maxRounds = 100)
         {
+            this.maxRounds = maxRounds;
+
             InititiveOrder = new List<Combatant>() {
-                new Combatant(mogwai) { InititativeValue = mogwai.InitiativeRoll(mogwaiDice), Dice = mogwaiDice },
-                new Combatant(monster) { InititativeValue = monster.InitiativeRoll(monsterDice), Dice = monsterDice }
+                new Combatant(mogwai) {
+                    IsHero = true,
+                    InititativeValue = mogwai.InitiativeRoll(mogwaiDice),
+                    Dice = mogwaiDice,
+                    Enemies = new List<Entity> { monster } },
+                new Combatant(monster) {
+                    InititativeValue = monster.InitiativeRoll(monsterDice),
+                    Dice = monsterDice,
+                    Enemies = new List<Entity> { mogwai }
+                }
             };
 
             InititiveOrder.OrderBy(s => s.InititativeValue).ThenBy(s => s.Entity.Dexterity);
-
-            Start();
         }
 
-        private void Start()
+        public void Start(bool verbose = true)
         {
-            
+            Console.WriteLine("Start Combat");
+            var hero = InititiveOrder.Where(p => p.IsHero).First();
+            Console.WriteLine($"{hero.Entity.Name}");
+
         }
     }
 }
