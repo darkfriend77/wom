5e175dda7c4224cb6d1e4e3fb20dcf5862dc69f6
darkfriend77 <darkfriend@swissonline.ch>
2018-08-31 19:16:53 +0200

    fixed error

--- WoMInterface/Game/Interaction/Unknown.cs
+++ WoMInterface/Game/Interaction/Unknown.cs
@@ -1,10 +1,15 @@
-﻿using WoMInterface.Game.Enums;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WoMInterface.Game.Enums;
 
 namespace WoMInterface.Game.Interaction
 {
     public class Unknown : Interaction
     {
-        public Unknown() : base(InteractionType.UNKNOWN)
+        public Unknown() : base(InteractionType.NONE)
         {
         }
     }
--- WoMInterface/Game/Model/Equipment/Armor.cs
+++ WoMInterface/Game/Model/Equipment/Armor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WoMInterface.Game.Model.Equipment
+namespace WoMInterface.Game.Model
 {
     class Armor
     {
