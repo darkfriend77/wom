5700f5b35b39bec3d1a443425e33d4fd2fa0eeae
darkfriend77 <darkfriend@swissonline.ch>
2018-09-04 22:37:38 +0200

    added basic wallet functionality added mirroraddress added mnemonic added nbitcoin added more

--- WoMApiTest/Node/BlockChain.cs
+++ WoMApiTest/Node/BlockChain.cs
@@ -1,22 +1,39 @@
-﻿using RestSharp;
+﻿using log4net;
+using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using WoMApiTest.Model.Node;
 
 namespace WoMApiTest.Node
 {
-    public class BlockChain
+    public class Blockchain
     {
-        private static BlockChain instance;
-        public static BlockChain Instance => instance ?? (instance = new BlockChain());
+        private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private MogwaiWallet wallet;
+
+        private static Blockchain instance;
+
+        public static Blockchain Instance => instance ?? (instance = new Blockchain());
 
         private RestClient client;
 
-        private BlockChain()
+        private Blockchain()
         {
-            client = new RestClient("https://cristof.crabdance.com/mogwai/");
+            client = new RestClient(ConfigurationManager.AppSettings["apiUrl"]);
+
+        }
+
+        public void Unlock(string password, string walletFile = null)
+        {
+            string path = walletFile ?? ConfigurationManager.AppSettings["walletFile"];
+
+            wallet = new MogwaiWallet(password, path);
         }
 
         public IRestResponse<Block> GetBlock(int value)
@@ -25,6 +42,21 @@ namespace WoMApiTest.Node
             request.AddUrlSegment("id", value.ToString());
             IRestResponse<Block> blockResponse = client.Execute<Block>(request);
             return blockResponse;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="mogwaiAddress"></param>
+        /// <param name="tryes"></param>
+        /// <returns></returns>
+        public bool CreateMogwaiAddress(out string mogwaiAddress, int tryes = 10)
+        {
+            mogwaiAddress = string.Empty;
+
+            //wallet.Secret.PubKey.Derivate();
+
+            return true;
         }
     }
 }
---
+++ WoMApiTest/Node/Wallet.cs
@@ -0,0 +1,202 @@
+﻿using log4net;
+using NBitcoin;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WoMApiTest.Tool;
+
+namespace WoMApiTest.Model.Node
+{
+    public class WalletFile
+    {
+        public string wifKey;
+
+        public byte[] chainCode;
+
+        public uint seed = 0;
+
+        public Dictionary<string, uint> EncryptedSecrets = new Dictionary<string, uint>();
+
+        public WalletFile(string wifKey, byte[] chainCode)
+        {
+            this.wifKey = wifKey;
+            this.chainCode = chainCode;
+        }
+
+    }
+
+    public class MogwaiKeys
+    {
+        private readonly ExtKey extkey;
+
+        private readonly Network network;
+
+        private readonly PubKey pubKey;
+
+        private readonly PubKey mirrorPubKey;
+
+        public string Address => pubKey.GetAddress(network).ToString();
+
+        public string MirrorAddress => mirrorPubKey?.GetAddress(network).ToString();
+
+        public bool HasMirrorAddress => mirrorPubKey != null;
+
+        public MogwaiKeys(ExtKey extkey, Network network)
+        {
+            this.extkey = extkey;
+            this.network = network;
+            this.pubKey = extkey.PrivateKey.PubKey;
+            Console.WriteLine(pubKey);
+            if (TryMirrorPubKey(extkey.PrivateKey.PubKey, out PubKey mirrorPubKey))
+            {
+                Console.WriteLine(mirrorPubKey);
+                this.mirrorPubKey = mirrorPubKey;
+            }
+        }
+
+        private bool TryMirrorPubKey(PubKey pubKey, out PubKey mirrorPubKey)
+        {
+            var pubKeyStr = Helpers.ByteArrayToString(pubKey.ToBytes());
+            var mirrorPubKeyStr =
+                   pubKeyStr.Substring(0, 8)
+                 + pubKeyStr.Substring(8, 2)
+                 + Helpers.ReverseString(pubKeyStr.Substring(10, 54))
+                 + pubKeyStr.Substring(64, 2);
+            var mirrorPubKeyBytes = Helpers.StringToByteArray(mirrorPubKeyStr);
+            try
+            {
+                mirrorPubKey = new PubKey(mirrorPubKeyBytes, false);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.InnerException);
+                mirrorPubKey = new PubKey(mirrorPubKeyBytes, true);
+                return true;
+            }
+        }
+
+    }
+
+    public class MogwaiWallet
+    {
+        private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly Network network = NBitcoin.Altcoins.Mogwai.Instance.Mainnet;
+
+        private readonly string path;
+
+        private WalletFile walletFile;
+
+        private ExtKey extKey;
+
+        private Mnemonic mnemo;
+        public string MnemonicWords
+        {
+            get
+            {
+                string mnemoStr = mnemo != null ? mnemo.ToString() : string.Empty;
+                mnemo = null;
+                return mnemoStr;
+            }
+        }
+
+        public MogwaiWallet(string password, string path)
+        {
+            this.path = path;
+
+            if (!TryReadFile(path, out walletFile))
+            {
+                mnemo = new Mnemonic(Wordlist.English, WordCount.Twelve);
+                extKey = mnemo.DeriveExtKey(password);
+                //extKey = new ExtKey();
+                var chainCode = extKey.ChainCode;
+                var encSecretWif = extKey.PrivateKey.GetEncryptedBitcoinSecret(password, network).ToWif();
+                walletFile = new WalletFile(encSecretWif, chainCode);
+                Persist(path, walletFile);
+            }
+            else
+            {
+                var masterKey = Key.Parse(walletFile.wifKey, password, network);
+                extKey = new ExtKey(masterKey, walletFile.chainCode);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="seed"></param>
+        /// <param name="persist"></param>
+        /// <returns></returns>
+        public MogwaiKeys GetMogwaiKeys(uint seed, bool persist = false)
+        {
+            var extKeyDerived = extKey.Derive(seed);
+            var wif = extKey.PrivateKey.GetWif(network);
+            if (persist)
+            {
+                var encSecretWif = extKey.PrivateKey.GetEncryptedBitcoinSecret(extKey.ToString(network), network).ToWif();
+                if (!walletFile.EncryptedSecrets.ContainsKey(encSecretWif))
+                {
+                    walletFile.EncryptedSecrets[encSecretWif] = seed;
+                }
+                Persist(path, walletFile);
+            }
+            return new MogwaiKeys(extKeyDerived, network);
+        }
+
+        public void Write()
+        {
+            Persist(path, walletFile);
+        }
+
+        private bool TryReadFile<T>(string path, out T obj)
+        {
+            obj = default(T);
+
+            if (!File.Exists(path))
+            {
+                return false;
+            }
+
+            try
+            {
+                var objDecrypted = Decrypt(File.ReadAllText(path));
+                obj = JsonConvert.DeserializeObject<T>(objDecrypted);
+                return true;
+            }
+            catch (Exception e)
+            {
+                _log.Error($"TryDBFile[{obj.GetType()}]: {e}");
+                return false;
+            }
+        }
+
+        private void IsValidPubKey()
+        {
+            //TODO
+            //Cryptography.ECDSA.Secp256K1Manager.IsCanonical()
+        }
+
+        private void Persist<T>(string path, T obj)
+        {
+            string objEncrypted = Encrypt(JsonConvert.SerializeObject(obj));
+            File.WriteAllText(path, objEncrypted);
+        }
+
+        private string Encrypt(string str)
+        {
+            return str;
+        }
+
+        private string Decrypt(string str)
+        {
+            return str;
+        }
+    }
+}
